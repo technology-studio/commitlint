4d912f70e89f2338710b055650ad6234c31779f6
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-10-11 20:12:21 +0200

    feat: add initial implementation (#1)
    * chore: add initial changes
    
    * feat: add config to index file and update package name
    
    * fix: fix setup and dependencies
    
    * feat: use txo semantic-release package
    
    * fix: fix regex pattern
    
    * fix: fix test script
    
    * chore: set package name back
    
    * feat: remove unused files and add bin
    
    * chore: add noop for test
    
    * Delete CHANGELOG.md
    
    * chore: remove unused files and allow bin in npm
    
    * chore: remove vscode folder
    
    * chore: add semantic release txo package

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2022-08-21T12:08:59+02:00
+ * @Copyright: Technology Studio
+**/
+
+const automaticCommitPattern = /^chore\(release\):.*\[skip ci\]/
+
+const commitlintConfig = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+
+  ignores: [
+    commitMsg => automaticCommitPattern.test(commitMsg)
+  ]
+}
+
+module.exports = commitlintConfig
